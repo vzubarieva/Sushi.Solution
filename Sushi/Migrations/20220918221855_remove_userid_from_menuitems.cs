a4dc81da7dc84b283715f80d17f60ee13d36c476
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-09-18 16:34:30 -0700

    add info into database and update some css

---
+++ Sushi/Migrations/20220918221855_remove_userid_from_menuitems.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sushi.Migrations
+{
+    public partial class remove_userid_from_menuitems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuItems_AspNetUsers_UserId",
+                table: "MenuItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MenuItems_UserId",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "MenuItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "MenuItems",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_UserId",
+                table: "MenuItems",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuItems_AspNetUsers_UserId",
+                table: "MenuItems",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Sushi/Models/MenuItem.cs
+++ Sushi/Models/MenuItem.cs
@@ -9,11 +9,7 @@ namespace Sushi.Models
         [Display(Name = "Menu item name")]
         public string MenuItemName { get; set; }
 
-        // public bool isChecked { get; set; }
-
         [Display(Name = "Menu item price")]
         public double MenuItemPrice { get; set; }
-        public string UserId { get; set; } // Foreign Key
-        public virtual ApplicationUser User { get; set; }
     }
 }
